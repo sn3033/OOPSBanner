18ab12b6daf303c1a601095eb3e5740da8aadee9
sn3033 <sn3033@srmist.edu.in>
2026-02-17 08:51:40 +0530

    Add files via upload

---
+++ UC1OOPSBannerApp.java
@@ -0,0 +1,14 @@
+/**
+ * UC1OOPSBannerApp - Use Case 1
+ * Prints the literal text "OOPS" to the console.
+ *
+ * @author Pradhanya
+ * @version 1.0
+ */
+public class UC1OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+
+}
